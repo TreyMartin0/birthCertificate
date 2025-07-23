c389cbb447ce5937acbe46e9f4bdb2949accb336
TreyMartin0 <tmarti90@calpoly.edu>
2025-07-23 09:23:36 -0400

    framework

--- force-app/main/default/lwc/speedTest/speedTest.js
+++ force-app/main/default/lwc/speedTest/speedTest.js
@@ -1,3 +1,30 @@
 import { LightningElement } from 'lwc';
 
-export default class SpeedTest extends LightningElement {}
+export default class SpeedTest extends LightningElement {
+    showSpeedTest = false;
+    testUrl = '';
+    downloadSpeed = '';
+    uploadSpeed = '';
+    
+    handleSpeedTestClick() {
+        this.showSpeedTest = true;
+        this.attachMessageListener();
+    }
+
+    attachMessageListener() {
+        window.addEventListener('message', this.handleTestResults.bind(this));
+    }
+
+    handleTestResults(event) {
+        if (event.origin !== '') {
+            return;
+        }
+        const result = event.data;
+        if (results) {
+            this.downloadSpeed = results.download || 'n/a'
+            this.uploadSpeed = results.upload || 'n/a'
+            this.showSpeedTest = false;
+        }
+        window.removeEventListener('message', this.handleTestResults.bind(this));
+    }
+}
